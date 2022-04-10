d8e920d60f92f69843aaa0c3f3d730294cd4e08d
verozubalexandr <90104414+verozubalexandr@users.noreply.github.com>
2022-04-10 17:06:50 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -124,7 +124,7 @@ public class Main {
         System.out.print("Enter message -> ");
         String inputString = enterString();
         byte foundLower = 0;
-        final int CHANGE_INDEX = (int) (Math.random()*8+1);
+        final int CHANGE_INDEX = (int) (Math.random()*9 + 1);
         char[] lowerCaseArray = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
                 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
                 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
